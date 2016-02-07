9b2500c8b7b9548516d2adcf7e270d8a13ff6606
Mikhail Paulyshka <me@mixaill.tk>
2016-02-07 14:15:03 +0300

    linux: use go from golang

--- update_linux.sh
+++ update_linux.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 killall Kotik
+export GODIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/../GODIR
 export GOPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/../GOPATH
+mkdir $GODIR
+wget -O $GODIR/go.tar.gz https://storage.googleapis.com/golang/go1.6rc2.linux-amd64.tar.gz
+tar -xzf $GODIR/go.tar.gz -C $GODIR
+export GOROOT=$GODIR/go
 git pull origin master
-go get
-go get -u
-go build .
+$GOROOT/bin/go get
+$GOROOT/bin/go build .
 ./Kotik -dev=false
